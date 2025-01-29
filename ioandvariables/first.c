ad879e0608cadd7eb8f83a26791d6de65793f7a8
AakashFrexy <as4938@njit.edu>
2025-01-29 17:53:13 -0500

    creating a new folder for chapter one

---
+++ ioandvariables/first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+    int c;
+    printf("enter a no. a");
+    scanf("%d", &a);
+    printf("enter a no. b");
+    scanf("%d", &b);
+    c = a+b;
+    printf("The sum is %d", c);
+    return 0;
+}
