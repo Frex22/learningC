106ebdd742ab7b03e8f5af9fffd0036ed7ffa46e
AakashFrexy <as4938@njit.edu>
2025-01-29 17:37:13 -0500

    changing gc2

--- gc2.c
+++ gc2.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
+#define MAX_MARKS 100
 int main() {
     int s1, s2, s3;
     float avg, total; 
     char grade;
-    printf("Add marks for subject 1\n");
-    scanf("%d", &s1);
-    printf("Add marks for subject 2\n");
-    scanf("%d", &s2);
-    printf("Add marks for subject \n");
-    scanf("%d", &s3);
+    int valid_input = 0;
+
+
+    do {
+        printf("Add marks for subject 1 (0-%d):", MAX_MARKS);
+        if (scanf("%d", s1) !=1 || s1<0 || s1 > MAX_MARKS); {
+            printf("invalid input for subject 1 enter valid no. ");
+
+            while (getchar() != '\n');
+            continue;
+        }
+
+        printf("Add marks for subject 2 (0-%d):", MAX_MARKS);
+        if (scanf("%d", s2) !=1 || s1<0 || s1 > MAX_MARKS); {
+            printf("invalid input for subject 1 enter valid no. ");
+
+            while (getchar() != '\n');
+            continue;
+        }
+
+        printf("Add marks for subject 3 (0-%d):", MAX_MARKS);
+        if (scanf("%d", s3) !=1 || s1<0 || s1 > MAX_MARKS); {
+            printf("invalid input for subject 1 enter valid no. ");
+
+            while (getchar() != '\n');
+            continue;
+        }
+
     total = s1+s2+s3;
-    avg = (total/3);
+    avg=total/3.0;
+
+    if (avg > MAX_MARKS) {
+        printf("Wrong Values avg exceeds %d", MAX_MARKS);
+        total = 0;
+        avg = 0.0;    
+    } else {
+        valid_input = 1;
+    }
+        
+    } while (!valid_input);
+
 
     if (avg > 100){
         printf(" Wrong values please try again");
